952b2ca45eb3f7e2ed8a24201b05377268c48d74
David Zhang <davidzhang1024@live.ca>
2013-02-16 19:34:29 -0500

    register on client

--- client.rb
+++ client.rb
@@ -1,17 +1,82 @@
+require 'optparse'
+require 'json'
+require 'pp'
 require 'net/http'
 
-HOST='http://ona.herokuapp.com/get'
-SLEEP=5
+def parse_config
+  parsed = config_path 
+  parsed_options = {}
+  begin
+    config_text = IO.read(parsed)
+    parsed_options = JSON.parse(config_text)
+  rescue
+  end
+
+  return parsed_options
+end
+
+def write_config(config)
+  File.open(config_path, 'w+') do |f|
+    f.puts(config.to_json)
+  end
+end
+
+def set_token(resp)
+  token = resp['token']
+  if(!token.nil?)
+    existing_config = parse_config 
+    existing_config['token'] = token
+    write_config(existing_config)
+  end
+
+end
+
+def make_request(url, flags)
+  begin
+    url = URI.parse(url)
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    
+   return JSON.parse(res.body)
+  rescue => ex
+    puts ex
+  end
+end
+
+def set_defaults(options)
+  options[:url] ||= 'http://ona.herokuapp.com/'
+end
+
+def config_path
+  "#{File.expand_path('~')}/.ona"
+end
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+  opts.on('-r', '--register', 'Register token') do
+    options[:register] = true
+    options[:path] = 'register'
+  end
+end
+
+optparse.parse!
+
+config = parse_config
+set_defaults(config)
+
+if(options[:register])
+  if(config['token'] && !config['force'])
+    puts "already registered: #{config['token']}"
+    exit
+  end
 
 
-url = URI.parse(HOST)
-req = Net::HTTP::Get.new(url.to_s)
+  resp = make_request(config[:url] + options[:path], {}) 
 
-loop do
-  res = Net::HTTP.start(url.host, url.port) {|http|
-    http.request(req)
-  }
-  puts res.body
-  puts "a"
-  sleep(SLEEP)
+  if(!resp.nil?)
+    set_token(resp)
+  end
 end
